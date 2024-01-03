89661349f18678ae19814118fec2dc1a478bc4b5
yukiny0811 <yukiny0811@gmail.com>
2024-01-04 05:07:37 +0900

    add rasterizerData temps

--- Sources/EasyMetalShader/MetalFuncs.swift
+++ Sources/EasyMetalShader/MetalFuncs.swift
@@ -252,4 +252,23 @@ void svd_2x2(
   U = transpose(R) * X;
 }
 """
+    
+    static let rasterizerData =
+"""
+struct RasterizerData {
+    float4 position [[ position ]];
+    float4 color;
+    float size [[point_size]];
+
+    float4 temp1;
+    float4 temp2;
+    float4 temp3;
+    float4 temp4;
+    float4 temp5;
+    float4 temp6;
+    float4 temp7;
+    float4 temp8;
+    float4 temp9;
+};
+"""
 }
