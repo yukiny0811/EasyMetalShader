8ad1278dcfe7a5efbebb42f711298f52077c8f7c
Yuki Kuwashima <yukiny0811@gmail.com>
2025-01-31 17:13:40 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", exact: "510.0.3"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.1"),
     ],
     targets: [
         .macro(
