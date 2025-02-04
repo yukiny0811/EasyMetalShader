2978f9e8bfe11aa7c2896d1d88b7fbb11fb06a3a
yukiny0811 <yukiny0811@gmail.com>
2025-02-04 22:15:36 +0900

    MTLCommandBuffer extension

--- Sources/EasyMetalShader/Pipelines/EMMetalDispatch.swift
+++ Sources/EasyMetalShader/Pipelines/EMMetalDispatch.swift
@@ -53,3 +53,27 @@ public class EMMetalDispatch {
         commandBuffer.waitUntilCompleted()
     }
 }
+
+extension MTLCommandBuffer {
+    
+    public func compute(_ process: @escaping (MTLComputeCommandEncoder) -> ()) {
+        let encoder = self.makeComputeCommandEncoder()!
+        process(encoder)
+        encoder.endEncoding()
+    }
+    
+    public func render(renderTargetTexture: MTLTexture, needsClear: Bool, process: @escaping (MTLRenderCommandEncoder) -> ()) {
+        let descriptor = MTLRenderPassDescriptor()
+        descriptor.colorAttachments[0].texture = renderTargetTexture
+        if needsClear {
+            descriptor.colorAttachments[0].loadAction = .clear
+        } else {
+            descriptor.colorAttachments[0].loadAction = .load
+        }
+        descriptor.colorAttachments[0].storeAction = .store
+        descriptor.colorAttachments[0].clearColor = .init(red: 0, green: 0, blue: 0, alpha: 0)
+        let encoder = self.makeRenderCommandEncoder(descriptor: descriptor)!
+        process(encoder)
+        encoder.endEncoding()
+    }
+}
